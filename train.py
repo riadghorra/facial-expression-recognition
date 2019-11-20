7f5e052d693c3cf9d940e8d324fac1687aaa975d
Arthur Lindoulsi <arthur@MacBook-Pro-de-Arthur-2.local>
2019-11-20 16:38:33 +0100

    Cuda ready code

--- train.py
+++ train.py
@@ -34,21 +34,31 @@ loss_function = nn.BCELoss().to(DEVICE)
 # =============================================================================
 # Train
 # =============================================================================
+def pixelstring_batch_totensor(psb):
+    out = torch.stack(tuple([pixelstring_to_tensor_vgg16(string) for string in psb]))
+    return out
+
+def emotion_batch_totensor(emb):
+    out = torch.stack(tuple([label_to_vector(em) for em in emb]))
+    return out
 
 def train(model, train_dataframe, test_dataframe, epochs, device, create_datatensor):
     optimizer = optim.Adam(model.parameters() , lr=LR)
-    train_data = create_datatensor(train_dataframe)
+    #train_data = create_datatensor(train_dataframe)
     
-    to_dataloader = [[train_data[i], train_dataframe["groundtruth"][i]] for i in range(len(train_data))]
+    to_dataloader = [[train_dataframe["pixels"][i], train_dataframe["emotion"][i]] for i in range(len(train_dataframe))]
     
     dataloader = torch.utils.data.DataLoader(to_dataloader, BATCH, shuffle=False, drop_last=True)
     model.train()
     print("debut du training")
     for epoch in tqdm(range(epochs), desc="Epochs"):
-        for batch, groundtruth in dataloader :
+        for pixelstring_batch, emotions_batch in dataloader :
+            groundtruth = emotion_batch_totensor(emotions_batch)
+            batch = pixelstring_batch_totensor(pixelstring_batch)
+            
             model.zero_grad()
             out = softmax(model(batch.to(DEVICE)))
-            labels = torch.tensor(groundtruth).to(DEVICE)
+            labels = groundtruth.to(DEVICE)
             loss = loss_function(out,labels)
             loss.backward()
             optimizer.step()
@@ -58,9 +68,9 @@ def train(model, train_dataframe, test_dataframe, epochs, device, create_dataten
         model.train()
         print()
         print("Epoch number : ", epoch+1)
-        print("Accuracy : ", round(100*float(acc),2), "%")
+        print("Accuracy sur le test : ", round(100*float(acc),2), "%")
         print("Loss test : ", float(loss_test))
-        print("Accuracy train: ", round(100*float(acctrain),2), "%")
+        print("Accuracy sur le train: ", round(100*float(acctrain),2), "%")
         print("Loss train : ", float(loss_train))
         print("_______________")
     return model.eval()
@@ -68,15 +78,23 @@ def train(model, train_dataframe, test_dataframe, epochs, device, create_dataten
 
 def evaluate(model, dataframe, create_datatensor):
     with torch.no_grad():
-        data = create_datatensor(dataframe)
-        out = softmax(model(data.to(DEVICE)))
-        labels = torch.tensor([]).to(DEVICE)
-        for tensor in dataframe["groundtruth"]:
-            labels = torch.cat((labels, tensor.unsqueeze(0))).to(DEVICE)
-        loss_value = loss_function(out,labels)
-        acc = sum(sum(out*labels))/(data.size()[0])
-        return acc, loss_value
-        
+        to_dataloader = [[dataframe["pixels"][i], dataframe["emotion"][i]] for i in range(len(dataframe))]
+        loss = torch.tensor(0.0).to(DEVICE)
+        compteur = torch.tensor(0.0).to(DEVICE)
+        error = torch.tensor(0.0).to(DEVICE)
+        dataloader = torch.utils.data.DataLoader(to_dataloader, BATCH, shuffle=False, drop_last=False)
+        for pixelstring_batch, emotions_batch in dataloader :
+            groundtruth = emotion_batch_totensor(emotions_batch)
+            batch = pixelstring_batch_totensor(pixelstring_batch)
+            
+            out = softmax(model(batch.to(DEVICE)))
+            labels = groundtruth.to(DEVICE)
+            loss += loss_function(out,labels)
+            compteur += torch.tensor(1.0).to(DEVICE)
+            error += (out*labels).sum()/torch.tensor(len(emotions_batch)).to(DEVICE)
+        loss_value = float(loss/compteur)
+        error = float(error/compteur)
+        return error, loss_value
         
 # =============================================================================
 # Main
@@ -86,8 +104,8 @@ def evaluate(model, dataframe, create_datatensor):
 def main(model, pixelstring_to_tensor, create_datatensor):
     print("creation du dataset")
     all_data = pd.read_csv(path, header = 0)[:sample]
-    all_data["tensors"] = all_data["pixels"].apply(pixelstring_to_tensor)
-    all_data["groundtruth"] = all_data["emotion"].apply(lambda x : label_to_vector(x, device = DEVICE))
+    #all_data["tensors"] = all_data["pixels"].apply(pixelstring_to_tensor)
+    #all_data["groundtruth"] = all_data["emotion"].apply(lambda x : label_to_vector(x, device = DEVICE))
     n_all = len(all_data)
     n_eval = int(eval_rate*n_all)
     n_test = int(test_rate*n_eval)
