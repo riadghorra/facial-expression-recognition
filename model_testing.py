d1dcb29180c3407c649100122ce93fe267baa551
Corentin Carteau <corentin.carteau.intern@birdie.care>
2020-03-11 15:13:43 +0100

    izi up 50p acc

--- model_testing.py
+++ model_testing.py
@@ -122,11 +122,10 @@ def test_on_annotated_csv(annotations_csv_path):
             (x, y, w, h) = face_coords
             face_image = crop_cv_img(cv_img, x, y, w, h)
             pil_face_image = pl.Image.fromarray(face_image).resize(config["resolution"])
+            pil_face_image = transforms.Grayscale(num_output_channels=1)(pil_face_image)
 
             pixels_list = np.array(pil_face_image).flatten().tolist()
-            if i < 10:
-                pil_test = string_to_pilimage(" ".join(map(str, pixels_list)))
-                pil_test.show()
+
             pixels.append(" ".join(map(str, pixels_list)))
         else:
             df.drop(index=i, inplace=True)
