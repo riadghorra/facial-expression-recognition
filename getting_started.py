580d17796db1428aa402f842d59db4d0575d04ad
Corentin Carteau <corentin.carteau.intern@birdie.care>
2020-03-11 13:57:45 +0100

    add getting started file

---
+++ getting_started.py
@@ -0,0 +1,44 @@
+from dataset_tools import string_to_pilimage
+from train import config
+import numpy as np
+import pandas as pd
+
+"""
+Load Images from FER.
+- config["path"] is the path to the dataset
+- config["data_column"] is "pixels" (if the dataset is not cropped) or "face" (if dataset is cropped).
+"""
+
+
+def load_cv_images_from_fer(output_type, nrows=None):
+    """
+    :param type: "CV" for open cv and "PIL" for PIL
+    :param nrows: nrows param for pd.read_csv
+    :return: list of images from the fer csv file in "type" format.
+    """
+    imgs = []
+    all_data = pd.read_csv(config["path"], header=0, nrows=nrows)
+    for i in range(len(all_data)):
+        pixels = all_data.loc[i][config["data_column"]]
+        pil_img = string_to_pilimage(pixels)
+
+        if output_type == "CV":
+            imgs.append(np.array(pil_img))
+        elif output_type == "PIL":
+            imgs.append(pil_img)
+        else:
+            raise Exception("Invalid type")
+
+    print("loaded images from csv")
+
+    return imgs
+
+
+def show_first_images():
+    imgs = load_cv_images_from_fer("PIL", nrows=3)
+
+    for img in imgs:
+        img.show()
+
+
+show_first_images()
